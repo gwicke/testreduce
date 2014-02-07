ad419ba4e37d9a60b61af22bc2ef2648e4e33945
Gabriel Wicke <wicke@wikidev.net>
2014-02-07 11:10:15 -0800

    Automatically init articles too

--- articles/initAll.sh
+++ articles/initAll.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+echo "Importing titles to test from a bunch of messages"
 for lang in ar de en es fr he hi it ja ko nl pl ru sv;do
 	echo "Importing $lang.."
 	node importJson.js -u testreduce -D testreduce --prefix "${lang}wiki" \

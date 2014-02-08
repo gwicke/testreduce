71801244262bbc9c92fa602e4a620a5daf635537
chandr19 <chandr19@illinois.edu>
2014-02-08 16:41:35 -0600

    added basic Cassandra backend code

--- CassandraBackend.js
+++ CassandraBackend.js
@@ -1,7 +1,46 @@
+var util = require('util'),
+  events = require('events'),
+  cass = require('node-cassandra-cql'),
+  consistencies = cass.types.consistencies,
+  uuid = require('node-uuid');
+
 // Constructor
-function CassandraBackend(config) {
-	this.config = config;
+function CassandraBackend(name, config, callback) {
+  events.EventEmitter.call(this);
+
+  var self = this;
+
+  this.name = name;
+  this.config = config;
+  // convert consistencies from string to the numeric constants
+  var confConsistencies = config.backend.options.consistencies;
+  this.consistencies = {
+    read: consistencies[confConsistencies.read],
+    write: consistencies[confConsistencies.write]
+  };
+
+  self.client = new cass.Client(config.backend.options);
+
+  var reconnectCB = function(err) {
+    if (err) {
+      // keep trying each 500ms
+      console.error('pool connection error, scheduling retry!');
+      setTimeout(self.client.connect.bind(self.client, reconnectCB), 500);
+    }
+  };
+  this.client.on('connection', reconnectCB);
+  this.client.connect();
+
+
+  //this.client.on('log', function(level, message) {
+  //  console.log('log event: %s -- %j', level, message);
+  //});
+  callback();
+
+  // Create queue - self.queue = <something>
 }
+
+util.inherits(CassandraBackend, events.EventEmitter);
 
 /**
  * Get the next title to test
@@ -13,9 +52,9 @@ function CassandraBackend(config) {
  * @returns object that serializes to JSON, for example
  * [ 'enwiki', 'some title', 12345 ]
  */
-CassandraBackend.prototype.getTest = function (commit) {
+CassandraBackend.prototype.getNextTest(commit) {
 	return [ 'enwiki', 'some title', 12345 ];
-};
+}
 
 /**
  * Add a result to storage
@@ -27,9 +66,8 @@ CassandraBackend.prototype.getTest = function (commit) {
  * @param result string (JUnit XML typically)
  * @return void
  */
-CassandraBackend.prototype.addResult = function(commit, result) {
-};
+CassandraBackend.prototype.addResult(commit, result) {
 
-// Node.js module exports. This defines what
-// require('./CassandraBackend.js'); evaluates to.
+}
+
 module.exports = CassandraBackend;

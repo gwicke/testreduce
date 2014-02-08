90e90ae1855f0b4524d9f5d98aa857e03874f1fe
Gabriel Wicke <wicke@wikidev.net>
2014-02-08 12:05:05 -0800

    CasandraBackend class

---
+++ CassandraBackend.js
@@ -0,0 +1,30 @@
+// Constructor
+function CassandraBackend() {
+}
+
+/**
+ * Get the next title to test
+ *
+ * commit is object {
+ *	hash: <git hash string>
+ *	timestamp: <git commit timestamp date object>
+ * }
+ * @returns object that serializes to JSON, for example
+ * [ 'enwiki', 'some title', 12345 ]
+ */
+CassandraBackend.prototype.getTest(commit) {
+	return [ 'enwiki', 'some title', 12345 ];
+}
+
+/**
+ * Add a result to storage
+ *
+ * @param commit object {
+ *	hash: <git hash string>
+ *	timestamp: <git commit timestamp date object>
+ * }
+ * @param result string (JUnit XML typically)
+ * @return void
+ */
+CassandraBackend.prototype.addResult(commit, result) {
+}

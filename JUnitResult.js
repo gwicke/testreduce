a85962a85fcf6f26d12eb44d29f75b51132662ef
Sebastian Wong <wngsebastian@gmail.com>
2014-03-04 00:48:49 -0600

    added tentatative count and result parsing into JUnitResult.js

--- JUnitResult.js
+++ JUnitResult.js
@@ -6,3 +6,13 @@ exports.parsePerfStats = function parsePerfStats(text) {
     }
     return perfstats;
 }
+
+exports.getResults = function getResults(test) {
+    // This assumes that the test will still be in raw request form,
+    // If not we can just skip the test.body and pass the results in straight.
+    result = test.body.results,
+    skipCount = result.match( /<skipped/g ),
+    failCount = result.match( /<failure/g ),
+    errorCount = result.match( /<error/g );
+    return {result: result, skipCount: skipCount, failCount: failCount, errorCount: errorCount}
+}

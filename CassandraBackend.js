d5e05a1d9a33befb3c1eaf1d3bb872191b48cd12
John <jychou@ucsd.edu>
2014-03-09 01:05:36 -0800

    getStatistics in cassbackend, fixed the timeout error

--- CassandraBackend.js
+++ CassandraBackend.js
@@ -90,6 +90,7 @@ function getTests(cb) {
             console.log( 'no seen commits, error in database' );
             cb(null, 0, 0);
         } else {
+
             // I'm not sure we need to have this, but it exists for now till we decide not to have it.
             for (var i = 0; i < results.rows.length; i++) {
                 this.testsList[results.rows[i]] = true;
@@ -136,8 +137,12 @@ function initTestPQ(commitIndex, numTestsLeft, cb) {
         cb(null);
     }
     var cql = 'select test, score, commit from test_by_score where commit = ?';
+    
 
-    this.client.execute(cql, [lastCommit], this.consistencies.write, queryCB.bind( this ));
+    //note to those who are working on the direct backend, I added this because now that the cql
+    //schema is changed, you can only do a range query on a blob 
+    myfake = new Buffer("0b5db8b91bfdeb0a304b372dd8dda123b3fd1ab6");
+    this.client.execute(cql, [myfake], this.consistencies.write, queryCB.bind( this ));
 }
 
 /**
@@ -197,35 +202,88 @@ CassandraBackend.prototype.getTest = function (commit, cb) {
  *
  * @param cb- (err, result), result is defined below
  *
+    
+
+
  */
-CassandraBackend.prototype.getStatistics = function(cb) {
+CassandraBackend.prototype.getStatistics = function(commit, cb) {
 
     /**
-     * @param results
-     *    object {
-     *       tests: <test count>,
-     *       noskips: <tests without skips>,
-     *       nofails: <tests without fails>,
-     *       noerrors: <tests without error>,
+     * @param result
+     *  Required results:
+        numtests-  
+        noerrors- numtests - ()
+        noskips- ()
+        nofails
+        latestcommit
+        crashes
+        beforelatestcommit
+        numfixes
+        numreg
      *
-     *       latestcommit: <latest commit hash>,
-     *       beforelatestcommit: <commit before latest commit>,
-     *
-     *       averages: {
-     *           errors: <average num errors>,
-     *           fails: <average num fails>,
-     *           skips: <average num skips>,
-     *           scores: <average num scores>
-     *       },
-     *
-     *       crashes: <num crashes>,
-     *       regressions: <num regressions>,
-     *       fixes: <num fixes>
-     *   }
-     *
+
+    how to compute a commit summary just by test_by_scores
+    1) use a commit and search through all test_by_scores
+    2) compute the amount of errors, skips, and fails 
+    num tests = num quered
+        - Go through each, and for every tests
+          If(score == 0) then noerrors++ ; nofails++; noskips++;
+          else IF(score > 1000000) -> do nothing
+          else If(score > 1000) (it's a fail = noerrors++) 
+          else If(score > 0 ) (it's a skip = noerrors++; no fails++) 
+    3) We have latest commit, num tests and For now, 
+    just mock the data for numreg, numfixes, and crashes and latest commit
+
      */
-    var results = {};
-    cb(null, results);
+
+ 
+    var args = [], 
+    results = {};
+
+    var cql = "select score from test_by_score where commit = ?"
+    args = args.concat([commit]);
+    this.client.execute(cql, args, this.consistencies.write, function(err, results) {
+        if (err) {
+            console.log("err: " + err);
+            cb(err);
+        } else if (!results || !results.rows) {
+            console.log( 'no seen commits, error in database' );
+            cb(null);
+        } else {
+            console.log("hooray we have data!: " + JSON.stringify(results, null,'\t'));
+            var noerrors = 0, nofails = 0, noskips = 0;
+            var numtests = results.rows.length;
+            async.each(results.rows, function(item, callback) {
+                console.log("item: " + JSON.stringify(item, null,'\t'));
+                var data = item[0];
+                if(data < 1000000) {
+                  if(data == 0) {
+                    noerrors++;
+                    noskips++;
+                    nofails++;
+                  } else if(data > 1000) {
+                    noerrors++;
+                  } else if(data > 0) {
+                    noerrors++;
+                    nofails++;
+                  }
+                }
+                callback();
+            }, function(err) {
+                results = {
+                    numtests: numtests,
+                    noerrors: noerrors,
+                    noskips: noskips,
+                    nofails: nofails
+                };
+                console.log("result: " + JSON.stringify(results, null,'\t'));
+              cb(null, results);
+
+            })
+        }
+    })
+    //var results = {};
+    
 }
 
 /**
